46e4027454edf5bcccdf9e4338e4e1172fb2aee9
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2025-01-10 08:09:17 -0300

    Comparacao e operador logico

--- introducao/index.js
+++ introducao/index.js
@@ -1,4 +1,11 @@
 let a = 10
 const b = 20
 
+console.log(a == b)
 console.log(a === b)
+console.log(a != b)
+console.log(a !== b)
+
+console.log(a != b && typeof a == 'string')
+console.log(a != b || typeof a == 'string')
+console.log(a == b || typeof a == 'string')
