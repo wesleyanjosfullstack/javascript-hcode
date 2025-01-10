e4d742895bf8d6d4063d874f15d5219d4921ac4c
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2025-01-10 08:20:48 -0300

    Estrutura condicao if e switch

--- introducao/index.js
+++ introducao/index.js
@@ -1,4 +1,22 @@
-let color = 'green'
+let color = 'yellow'
 
 if (color === 'green') console.log('Siga em frente !')
-    else console.log('Pare !')
+else if (color === 'yellow') console.log('Atenção !')
+else if (color === 'red') console.log('Pare !')
+
+color = 'green'
+
+switch(color) {
+    case 'green':
+        console.log('Siga em frente !')
+        break
+    case 'yellow':
+        console.log('Atenção !')
+        break
+    case 'red':
+        console.log('Pare !')
+        break
+    default:
+        console.log('Muita atenção !')
+        break
+}
