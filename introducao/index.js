5957ea487174e0723f705be6651bcb89d7bf5733
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2025-01-10 08:38:55 -0300

    Funcao anonima

--- introducao/index.js
+++ introducao/index.js
@@ -1,8 +1,5 @@
-function calc(x , y, op) {
+console.log(
+    (function(x, y, op) {
     return eval(`${x} ${op} ${y}`)
-}
-
-let r = calc(1, 2, '+')
-console.log(r)
-r = calc(1, 2, '*')
-console.log(r)
+})(1, 2, '*')
+)
