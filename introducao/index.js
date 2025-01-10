02c5123c15cc5a9fa80c9d9d74fc6c59e56d3579
wesleyanjosfullstack <wesleyanjosfullstack@gmail.com>
2025-01-10 09:02:14 -0300

    Percorrendo um array

--- introducao/index.js
+++ introducao/index.js
@@ -1,5 +1,5 @@
 let cars = ['palio 98', 'toro', 'uno', 10, true, new Date(), function(){}]
-console.log(cars)
-console.log(`Itens no array: ${cars.length}`)
-console.log(cars[0])
-console.log(cars[5].getFullYear())
+
+cars.forEach(function(value, index){
+    console.log(index, value)
+})
