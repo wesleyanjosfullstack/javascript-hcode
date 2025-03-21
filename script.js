c7002504ac477ed981394f0e5da4a61ea53afc27
WESLEY ANJOS <wesleyanjosfullstack@gmail.com>
2025-03-21 01:01:46 -0300

    utilizando operadores lógicos

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ var hello = 'Hello world !'
 console.log(hello)
 
 let a = 10
-const b = 20
-console.log(a == b)
+const b = '10'
+console.log(a == b && typeof b == 'string')
+console.log(a == b && typeof a == 'string')
+console.log(a == b || typeof a == 'string')
