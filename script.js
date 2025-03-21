9476f0d197a92ff53ce24526ee7a800163340a35
WESLEY ANJOS <wesleyanjosfullstack@gmail.com>
2025-03-21 01:05:48 -0300

    utilizando condição if else

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const b = '10'
 console.log(a == b && typeof b == 'string')
 console.log(a == b && typeof a == 'string')
 console.log(a == b || typeof a == 'string')
+
+let color = 'yellow'
+
+if (color === 'green') {
+    console.log('siga !')
+} else if (color === 'yellow') {
+    console.log('atenção !')
+} else {
+    console.log('pare !')
+}
